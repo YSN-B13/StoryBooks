ff27e2f7a444f99256619d916669872703bf1d92
YSN-B13 <yagamiilight1986@gmail.com>
2026-07-01 16:34:39 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,13 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected successfully: ${conn.connection.host}`);
-    } catch (err) {
-        console.log("Error with connecting with the MongoDB", error);
+    } catch (error) {
+        console.log("Error with connecting with the MongoDB", error.message);
         process.exit(1);
     }
 };
