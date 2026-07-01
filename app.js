c7ec0d33f891f82df041d1fb5fe19a2f54968ad5
YSN-B13 <yagamiilight1986@gmail.com>
2026-07-01 17:57:54 +0100

    Add session store with MongoDB and Passport authentication middleware

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
 const path = require("path");
+const passport = require("passport");
+const session = require("express-session");
 const connectDB = require("./config/db");
 
 // Load config
 dotenv.config();
+
+// Passport config
+require("./config/passport")(passport);
 
 // Connect to database
 connectDB();
@@ -16,6 +21,20 @@ const app = express();
 // Body parser middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Sessions middleware
+app.use(
+    session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+    }),
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Logging
 if (process.env.NODE_ENV === "development") {
