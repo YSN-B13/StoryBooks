073fc4125763afa0a2132d3edfd085bb506fd45e
YSN-B13 <yagamiilight1986@gmail.com>
2026-07-02 02:54:51 +0100

    Complete StoryBooks app with Google OAuth, sessions, and Handlebars

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require("path");
 const passport = require("passport");
 const session = require("express-session");
 const { MongoStore } = require("connect-mongo");
+const methodOverride = require('method-override');
 const connectDB = require("./config/db");
 
 // Load config
@@ -24,6 +25,19 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Method override
+app.use(
+    methodOverride(function (req, res) {
+        if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        let method = req.body._method;
+        delete req.body._method;
+        return method;
+        }
+    }),
+);
+
+
 // Sessions middleware
 app.use(
     session({
@@ -40,13 +54,35 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Set global var
+app.use(function (req, res, next) {
+    res.locals.user = req.user || null;
+    next();
+});
+
 // Logging
 if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
 };
 
+// Handlebars Helpers
+const {
+    formatDate,
+    stripTags,
+    truncate,
+    editIcon,
+    select,
+} = require('./helpers/hbs');
+
 // Handlebars
 app.engine('.hbs', exphbs.engine({
+    helpers: {
+        formatDate,
+        stripTags,
+        truncate,
+        editIcon,
+        select,
+    },
     defaultLayout: 'main', 
     extname: '.hbs'
 }));
@@ -58,6 +94,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Routes
 app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
+app.use("/stories", require("./routes/stories"));
 
 const PORT = process.env.PORT || 3000;
 
